03bc80b7a7bb2945a35294bb64fb4a91c73eedcc
Jun19204 <thomas19204@gmail.com>
2026-09-10 02:13:04 +0900

    fix: 버퍼 오버플로우 예방 코드로 수정

--- examples/iostream.c
+++ examples/iostream.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,29 +13,30 @@ int main(void) {
   // 1. 문자열 입력
   char str[STR_SIZE];
   printf("문자열 입력: ");
-  if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-    buffer[strcspn(buffer, "\n")] = '\0';
-    snprintf(str, sizeof(str), "%s", buffer);
+  if (fgets(str, sizeof str, stdin) == NULL) {
+    return EXIT_FAILURE;
   }
+  str[strcspn(str, "\n")] = '\0';
 
   // 2. 문자 입력
   char c;
   printf("문자 입력: ");
-  if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-    c = buffer[0];
+  if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+    return EXIT_FAILURE;
   }
+  c = buffer[0];
 
   // 3. 숫자 입력
-  int num;
+  long num;
+  char* endptr;
+  errno = 0;
   printf("정수 입력: ");
-  if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-    if (sscanf(buffer, "%d", &num) != 1) {
-      fprintf(stderr, "숫자 파싱 실패");
-      return EXIT_FAILURE;
-    }
+  if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+    return EXIT_FAILURE;
   }
+  num = strtol(buffer, &endptr, 10);
 
 
-  printf("%s %c", str, c);
+  printf("%s %c %ld", str, c, num);
   return EXIT_SUCCESS;
 }
